572d2eced134cb9d5bcbcb5d9bf7bd15feb75c49
Dmitriy Filatov <filatov-d@mail.ru>
2023-08-29 11:30:56 +0300

    hw13 script "student_use" was added catching

--- hw13/student_use.py
+++ hw13/student_use.py
@@ -3,11 +3,22 @@
 с выводом подробной информации. Поднимайте исключения внутри основного кода. Например нельзя создавать прямоугольник
 со сторонами отрицательной длины.
 """
-
+from student_exeptions import StudentNameError, InvalidScoreError, InvalidSubjectError
 from student import Student
 
-student = Student("дмитрий Филатов", "filatov.csv")
-student.add_score("rus_lang", 5)
-student.add_test_result("math", 95)
-print(student.average_test_score("math"))
-print(student.average_score())
+try:
+    student = Student("Дмитрий Филатов", "filatov.csv")
+    student.add_score("math", 5)
+    student.add_test_result("math", 94)
+    print(student.average_test_score("math"))
+    print(student.average_score())
+
+except InvalidSubjectError as e:
+    print("Error:", e)
+
+except StudentNameError as e:
+    print("Error:", e)
+
+except InvalidScoreError as e:
+    print("Error:", e)
+
